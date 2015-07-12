bf7fbd830aa41263846fd978bd27357b3b8927e9
Nivvedan <nivvedan@gmail.com>
2015-07-12 16:15:39 -0400

    Using a backslash instead of forward slash for commands

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -2,8 +2,8 @@ $("html").on("DOMSubtreeModified", "div.block-compose div.input", function(e) {
 		var text = e.target.innerText;
 
 		$.each(shortcuts, function (i, shortcut) {
-			if (text.search("/" + shortcut["shortcut"] + " ") != -1) {
-				text = text.replace("/" + shortcut["shortcut"], shortcut["text"])
+			if (text.search("\\" + shortcut["shortcut"] + " ") != -1) {
+				text = text.replace("\\" + shortcut["shortcut"], shortcut["text"])
 				e.target.innerText = text;
 			}
 		});
