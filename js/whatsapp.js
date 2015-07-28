1ff593c52532b1ca1b9f36898407f9bf4309b657
Nivvedan <nivvedan@gmail.com>
2015-07-27 23:36:30 -0400

    Fixed some unexpected behavior with String.search

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -2,7 +2,7 @@ $("html").on("DOMSubtreeModified", "div.block-compose div.input", function(e) {
 		var text = e.target.innerText;
 
 		$.each(shortcuts, function (i, shortcut) {
-			if (text.search("\\" + shortcut["shortcut"] + " ") != -1) {
+			if (text.indexOf("\\" + shortcut["shortcut"] + " ") != -1) {
 				text = text.replace("\\" + shortcut["shortcut"], shortcut["text"])
 				e.target.innerText = text;
 			}
